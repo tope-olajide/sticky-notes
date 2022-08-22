72e93726c5c96b978db5b248e735baa4a0c6caa5
tope-olajide <tope-olajide@users.noreply.github.com>
2022-08-23 00:37:39 +0100

    Create The Footer

---
+++ pages/components/Footer.tsx
@@ -0,0 +1,14 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+const Footer = () => {
+    return (
+        <>
+       <footer>
+<p>Developed with <FontAwesomeIcon icon={faHeart} size={"xs"} /> by Temitope.js</p>
+        </footer>
+        </>
+    )
+}
+export default Footer
