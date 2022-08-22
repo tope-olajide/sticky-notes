9c110558f0f5f9149c4033591efce9e47b05e4ad
tope-olajide <tope-olajide@users.noreply.github.com>
2022-08-23 00:45:41 +0100

    Create MainNavigationBar.tsx

---
+++ pages/MainNavigationBar.tsx
@@ -0,0 +1,15 @@
+
+const MainNavigationBar = () => {
+
+    return (
+        <>
+            <div className="auth-navbar">
+                <h1>Sticky Note</h1>
+                <div className="menu">
+                    <button className="logout-button">Logout</button>
+                </div>
+            </div>
+        </>
+    )
+}
+export default MainNavigationBar
