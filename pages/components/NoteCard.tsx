ba0b40fbc9700a0fa84edafc265c680e5e5d9520
tope-olajide <tope-olajide@users.noreply.github.com>
2022-08-23 00:45:52 +0100

    Update NoteCard.tsx

--- pages/components/NoteCard.tsx
+++ pages/components/NoteCard.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus, faWindowMaximize, faTrash, faSave } from '@fortawesome/free-solid-svg-icons'
-    
+
 enum Theme {
     Yellow = "yellow",
     Green = "green",
@@ -12,27 +12,27 @@ enum Theme {
 }
 
 interface IProps {
-        createNote():unknown;
-        changeColor( noteId:string, color:string):unknown;
-        noteId:string;
-        color:Theme;
-        toggleFullscreen(noteId:string):unknown;
-        isMaximized:boolean;
-        deleteNote(noteId:string):unknown;
-    }
-const NoteCard: React.FC<IProps> = ( { createNote, changeColor, noteId, color, toggleFullscreen, isMaximized, deleteNote} ) => {
+    createNote(): void;
+    changeColor(noteId: string, color: string): void;
+    id: string;
+    color: Theme;
+    toggleFullscreen(noteId: string): void;
+    isMaximized: boolean;
+    deleteNote(noteId: string): void;
+}
+const NoteCard: React.FC<IProps> = (IProps) => {
 
     return (
         <>
-            <div className={isMaximized?"card-maximized":"card"} id={color}>
+            <div className={IProps.isMaximized ? "card-maximized" : "card"} id={IProps.color}>
                 <div className="card-header">
                     <div className="icon-container">
-                        <div className="left-icon"><div className="icon" onClick={()=>createNote()}><FontAwesomeIcon icon={faPlus} /></div>
+                        <div className="left-icon"><div className="icon" onClick={() => IProps.createNote()}><FontAwesomeIcon icon={faPlus} /></div>
                         </div>
                         <div className="right-icon">
                             <div className="icon"><FontAwesomeIcon icon={faSave} /></div>
-                            <div  className="icon"onClick={()=>toggleFullscreen(noteId)}><FontAwesomeIcon icon={faWindowMaximize} /></div>
-                            <div className="icon" onClick={()=>deleteNote(noteId)} ><FontAwesomeIcon icon={faTrash} /></div>
+                            <div className="icon" onClick={() => IProps.toggleFullscreen(IProps.id)}><FontAwesomeIcon icon={faWindowMaximize} /></div>
+                            <div className="icon" onClick={() => IProps.deleteNote(IProps.id)} ><FontAwesomeIcon icon={faTrash} /></div>
                         </div>
                     </div>
                 </div>
@@ -43,13 +43,13 @@ const NoteCard: React.FC<IProps> = ( { createNote, changeColor, noteId, color, t
                 </div>
                 <div className="card-footer">
                     <div className="theme-color-container">
-                        <div className="theme-color yellow" onClick={()=>changeColor(noteId, Theme.Yellow)} > </div>
-                        <div className="theme-color green"  onClick={()=>changeColor(noteId, Theme.Green)}></div>
-                        <div className="theme-color pink" onClick={()=>changeColor(noteId, Theme.Pink)}></div>
-                        <div className="theme-color purple" onClick={()=>changeColor(noteId, Theme.Purple)}></div>
-                        <div className="theme-color blue" onClick={()=>changeColor(noteId, Theme.Blue)}></div>
-                        <div className="theme-color gray" onClick={()=>changeColor(noteId, Theme.Gray)}></div>
-                        <div className="theme-color charcoal" onClick={()=>changeColor(noteId, Theme.Charcoal)}></div>
+                        <div className="theme-color yellow" onClick={() => IProps.changeColor(IProps.id, Theme.Yellow)} > </div>
+                        <div className="theme-color green" onClick={() => IProps.changeColor(IProps.id, Theme.Green)}></div>
+                        <div className="theme-color pink" onClick={() => IProps.changeColor(IProps.id, Theme.Pink)}></div>
+                        <div className="theme-color purple" onClick={() => IProps.changeColor(IProps.id, Theme.Purple)}></div>
+                        <div className="theme-color blue" onClick={() => IProps.changeColor(IProps.id, Theme.Blue)}></div>
+                        <div className="theme-color gray" onClick={() => IProps.changeColor(IProps.id, Theme.Gray)}></div>
+                        <div className="theme-color charcoal" onClick={() => IProps.changeColor(IProps.id, Theme.Charcoal)}></div>
                     </div>
                 </div>
             </div>
