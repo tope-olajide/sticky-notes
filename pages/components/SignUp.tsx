3b17dd38a961d1386c1cd8b69aa92ddb8fbd0c33
tope-olajide <tope-olajide@users.noreply.github.com>
2022-08-24 12:02:36 +0100

    Update SignUp.tsx

--- pages/components/SignUp.tsx
+++ pages/components/SignUp.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
+import { useState, ChangeEvent, FormEvent } from 'react'
 import Footer from './Footer'
 import AuthNavigationbar from './AuthNavigationBar';
 const SignUp = () => {
 
   const [userInputs, setUserInputs] = useState({})
 
-  const saveUserInputs = (e: any) => {
-    setUserInputs({ ...userInputs, [e.target.name]: e.target.value })
+  const saveUserInputs = (event: ChangeEvent<HTMLInputElement>) => {
+    setUserInputs({ ...userInputs, [event.target.name]: event.target.value })
     console.log(userInputs)
   }
 
-  const handleFormSubmit = async (event: any) => {
+  const handleFormSubmit = async (event: FormEvent) => {
     event.preventDefault()
     console.log(userInputs)
   };
@@ -22,10 +22,10 @@ const SignUp = () => {
         <div className="auth-container">
           <form className="auth-form">
             <h3>Create Acount</h3>
-            <input type='text' name="fullname" placeholder="fullname" onChange={(e) => saveUserInputs(e)} />
-            <input type='text' name="username" placeholder="username" onChange={(e) => saveUserInputs(e)} />
-            <input type='email' name="email" placeholder="email" onChange={(e) => saveUserInputs(e)} />
-            <input type='password' name="password" placeholder="password" onChange={(e) => saveUserInputs(e)} />
+            <input type='text' name="fullname" placeholder="fullname" onChange={(event) => saveUserInputs(event)} />
+            <input type='text' name="username" placeholder="username" onChange={(event) => saveUserInputs(event)} />
+            <input type='email' name="email" placeholder="email" onChange={(event) => saveUserInputs(event)} />
+            <input type='password' name="password" placeholder="password" onChange={(event) => saveUserInputs(event)} />
             <button onClick={handleFormSubmit}>Create Account</button>
           </form>
         </div>
