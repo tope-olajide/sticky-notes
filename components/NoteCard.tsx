96f5e8083ff14d19cda102c1616ed3dc8a0ef185
tope-olajide <tope-olajide@users.noreply.github.com>
2022-09-26 13:30:41 +0100

    Add Feature That Allow User To Create A New Note

--- components/NoteCard.tsx
+++ components/NoteCard.tsx
@@ -12,7 +12,7 @@ enum Theme {
 }
 
 interface IProps {
-    createNote(): void;
+    createNote(noteId: string): void;
     changeColor(noteId: string, color: string): void;
     id: string;
     color: Theme;
@@ -28,9 +28,11 @@ interface IProps {
 
 const NoteCard: React.FC<IProps> = (props) => {
     const [timer, setTimer] = useState<NodeJS.Timeout>()
+    const [noteContents, setNoteContents] = useState('');
     const handleContentChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
         clearTimeout(timer)
         const newTimer: NodeJS.Timeout = setTimeout(() => {
+            setNoteContents(event.target.value)
             props.saveUserNote(props.id, props.color, event.target.value, props.isSaved)
         }, 2000)
         setTimer(newTimer)
@@ -41,11 +43,11 @@ const NoteCard: React.FC<IProps> = (props) => {
                 <div className="card-header">
                     <div className="icon-container">
                         <div className="left-icon">
-                            <div className="icon" onClick={() => props.createNote()}><FontAwesomeIcon icon={faPlus} /></div>
+                            <div className="icon" onClick={() => props.createNote(props.id)}><FontAwesomeIcon icon={faPlus} /></div>
                             <div className="icon" >{props.isSaving ? <FontAwesomeIcon icon={faSpinner} spin />:props.isError ? <FontAwesomeIcon icon={faExclamationTriangle} />:props.isSaved ? <FontAwesomeIcon icon={faCheck} /> : null}</div>
                         </div>
                         <div className="right-icon">
-                            <div className="icon"><FontAwesomeIcon icon={faSave} /></div>
+                            <div className="icon" onClick={()=>props.saveUserNote(props.id, props.color, noteContents, props.isSaved)}><FontAwesomeIcon icon={faSave} /></div>
                             <div className="icon" onClick={() => props.toggleFullscreen(props.id)}><FontAwesomeIcon icon={faWindowMaximize} /></div>
                             <div className="icon" onClick={() => props.deleteNote(props.id)} ><FontAwesomeIcon icon={faTrash} /></div>
                         </div>
